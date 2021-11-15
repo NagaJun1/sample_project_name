157b78c243936268ea60237660b952a3feeea5d3
NagaJun1 <jumpei603@gmail.com>
2021-11-15 23:22:23 +0900

    ディレクトリを挟む必要が無かったため削除

--- src/main/java/com/example/sample_project_name/ChildPage.java
+++ src/main/java/com/example/sample_project_name/ChildPage.java
@@ -1,4 +1,4 @@
-package com.example.sample_project_name.children;
+package com.example.sample_project_name;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/example/sample_project_name/SampleProjectNameApplication.java
+++ src/main/java/com/example/sample_project_name/SampleProjectNameApplication.java
@@ -12,5 +12,4 @@ public class SampleProjectNameApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SampleProjectNameApplication.class, args);
 	}
-
 }
